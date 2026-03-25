b81f4af3c29a7d24455fcd9186bb970419fff128
Sinoy <pramitghose777@gmail.com>
2026-03-25 10:39:55 +0530

    again

--- back/src/schema/user.js
+++ back/src/schema/user.js
@@ -1,14 +1,28 @@
-const mongoose = require('mongoose')
 
-const userSchemaRaw = new mongoose.Schema({
-  name: { type: String, required: true },
-  role: {
-    type: String,
-    enum: ['learner', 'educator', 'admin'],
-    default: 'learner',
-    required: true,
-  },
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ['teacher', 'student' , 'admin'],
+        required: true
+    }
 }, { timestamps: true });
+const holisticUserSchema = new mongoose.Schema({
+    User : { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    grade: { type: String, required: true },
+    subjects: [{ type: String }],
+    performance: {
+        math: { type: Number },
+        science: { type: Number },
+        literature: { type: Number },
+        
+    }
+}, { timestamps: true });
+const User = mongoose.model('User', userSchema);
 
-
-module.exports = { userSchema: mongoose.module('User', userSchemaRaw), userSchemaRaw };
+module.exports = User;
